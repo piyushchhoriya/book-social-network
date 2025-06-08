03f67929d70e1ef1fdea79d8e1b41f5c64c4a44e
Piyush Chhoriya <piyushchhoriya98@gmail.com>
2025-06-08 18:13:38 -0400

    Role and Token entities code commit

---
+++ book-network/src/main/java/com/piyush/book/role/RoleRepository.java
@@ -0,0 +1,8 @@
+package com.piyush.book.role;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+}
